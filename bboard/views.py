da7ab53f461807a583a4b16945f163af207ec235
flash207111 <vadim2071@inbox.ru>
2019-08-21 23:12:16 +0500

    just working

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Bb
 
 def index(request):
-    return HttpResponse("<h1>Hi! Everybody!</h1>")
+    list = 'Список объявлений\r\n\r\n\r\n'
+    for b in Bb.objects.order_by('-published'):
+        list += str(b.id) + ' ' + b.title + '\r\n' + b.content + '  Опубликовано: ' + str(b.published) + '\r\n\r\n'
+    return HttpResponse(list, content_type = 'text/plain; charset=utf-8')
+
