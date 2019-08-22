fe25099db69e5f9c919729dff38f203a90566927
flash207111 <vadim2071@inbox.ru>
2019-08-22 23:19:28 +0500

    end 1 chapter

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Bb
 
 def index(request):
-    list = 'Список объявлений\r\n\r\n\r\n'
-    for b in Bb.objects.order_by('-published'):
-        list += str(b.id) + ' ' + b.title + '\r\n' + b.content + '  Опубликовано: ' + str(b.published) + '\r\n\r\n'
-    return HttpResponse(list, content_type = 'text/plain; charset=utf-8')
+    # queryset = Bb.objects.order_by('-published') # list of objects
+    queryset = Bb.objects.all() # list of objects
+    context = {
+        'object_list': queryset
+    }
+    return render(request, 'bboard/index.html', context)
 
