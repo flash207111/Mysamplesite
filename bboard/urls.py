8860b998fe533ce7bfa6cc6a9525ca545a1c2ba1
flash207111 <vadim2071@inbox.ru>
2019-09-14 10:08:57 +0500

    just 130919

--- bboard/urls.py
+++ bboard/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index' ),
     path('<int:rubric_id>/', views.by_rubric, name = 'by_rubric' ),
-    path('create/', views.BbCreateView.as_view(), name = 'create' ),
+    # path('create/', views.BbCreateView.as_view(), name = 'create' ),
+    # path('add/', views.add, name='add'),
+    # path('add/save', views.add_save, name='add')
+    path('add/', views.add_and_save, name='add')
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 from .models import Bb, Rubric
 from .forms import BbForm
+
 
 def index(request):
     bbs = Bb.objects.all()
@@ -25,16 +27,45 @@ def by_rubric(request, rubric_id):
     return render(request, 'bboard/by_rubric.html', context)
 
 
-class BbCreateView(CreateView):
-    template_name = 'bboard/create.html'
-    form_class = BbForm
-    success_url = reverse_lazy('index')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['rubrics'] = Rubric.objects.all()
-        return context
+# class BbCreateView(CreateView):
+#     template_name = 'bboard/create.html'
+#     form_class = BbForm
+#     success_url = reverse_lazy('index')
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['rubrics'] = Rubric.objects.all()
+#         return context
 # Базовый класс реализует функциональность по созданию формы, выводу ее на экран с применением
 # указанного шаблона, получению занесенных в форму данных, проверке их на корректность, сохранению их
 # в новой записи модели и перенаправлению в случае успеха на интернет-адрес, который мы зададим. Вот
 # какой полезный класс этот CreateView!
+
+# def add(request):
+#     bbf = BbForm()
+#     context = {'form': bbf}
+#     return render(request, 'bboard/create.html', context)
+#
+# def add_save(request):
+#     bbf = BbForm(request.POST)
+#     if bbf.is_valid():
+#         bbf.save()
+#         return HttpResponseRedirect(reverse('by_rubric',
+#                                             kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+#     else:
+#         context = {'form': bbf}
+#         return render(request, 'bboard/create.html', context)
+
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('by_rubric', kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'bboard/create.html', context)
